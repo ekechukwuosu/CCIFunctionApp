8871b3843ea1eab124a3b87e92b07d9c06d72488
Eke Osu <eke.osu@claimscorpinc.com>
2024-07-10 13:47:05 -0400

    commit resilience

--- CCIFunctionApp/Program.cs
+++ CCIFunctionApp/Program.cs
@@ -1,8 +1,12 @@
 using CCIFunctionApp;
 using CCIFunctionApp.Middleware;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http.Resilience;
+using Polly;
+using System.Threading.RateLimiting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication(app =>
@@ -15,6 +19,21 @@ var host = new HostBuilder()
         services.ConfigureFunctionsApplicationInsights();
         services.AddMemoryCache();
         services.AddSingleton<CacheService>();
+        services.AddHttpClient("my-client")
+        .AddResilienceHandler("my-pipeline", builder =>
+        {
+            builder.AddRetry(new HttpRetryStrategyOptions
+            {
+                MaxRetryAttempts = 4,
+                Delay = TimeSpan.FromSeconds(2),
+                BackoffType = DelayBackoffType.Exponential
+            });
+            builder.AddRateLimiter(new HttpRateLimiterStrategyOptions
+            {
+                DefaultRateLimiterOptions = { PermitLimit = 4, QueueLimit = 2, QueueProcessingOrder = QueueProcessingOrder.OldestFirst },
+            });
+            builder.AddTimeout(TimeSpan.FromSeconds(5));
+        });
     })
     .Build();
 
